1e4624758d9fe020ae803a2d42cb27420a7ef0b4
Pavan <pavankumarsimma08@gmail.com>
2024-02-27 16:03:32 +0530

    added the function for the students registering for events

--- server/controls/student_controls.js
+++ server/controls/student_controls.js
@@ -81,3 +81,67 @@ export const registerAsVolunteer = async(req, res, next)=>{
     result = `${roll} - ${eventid}`;
     return res.status(200).json(result);
 };
+
+
+export const registerForEvent = async(req, res, next)=>{
+    const { roll, name, hid, password, eventid } = req.body;
+    const client = await pool.connect();
+    let result;
+    let pid = 0;
+    let collegeid = 1;
+    try{
+        if (roll == undefined || eventid==undefined || hid == undefined || name==undefined || password==undefined){
+            return res.status(400).json({message:"undefined data given"});
+        }
+        await client.query('BEGIN');
+        let queryText = `select * from student_participants where student_participants.roll='${roll}'`;
+        result = await client.query(queryText);
+        if (result.rows.length == 0){
+            queryText = `select pid from participants order by participants.pid desc limit 1`;
+            result = await client.query(queryText);
+            if (result.rows.length == 0){
+                pid = 101 + result.rows.length;
+            }
+            else {
+                pid = result.rows[0].pid +1;
+            }
+            queryText = `select * from participants where participants.pid=${pid}`;
+            result = await client.query(queryText);
+            if (result.rows.length == 0){
+                queryText = `insert into participants values (${pid}, '${name}', ${collegeid}, ${hid}, '${password}')`;
+                result = await client.query(queryText);
+            }
+            
+            queryText = `insert into student_participants values('${roll}', ${pid})`;
+            result = await client.query(queryText);
+        }
+        else {
+            pid = result.rows[0].pid;
+        }
+        
+        queryText = `select * from event_participants where event_participants.eventid=${eventid} and event_participants.pid=${pid}`;
+        result = await client.query(queryText);
+        if (result.rows.length != 0){
+            // already exists
+            return res.status(500).json({message:"Already exists"});
+        }
+        else {
+            // ok
+            queryText = `insert into event_participants values(${eventid}, ${pid})`;
+            result = await client.query(queryText);
+        }
+
+        await client.query('COMMIT');
+    }catch(e){
+        await client.query('ROLLBACK');
+        console.log(e);
+    }
+    finally{
+        client.release();
+    }
+    if(!result){
+        return res.status(310).json({message:"invalid data"});
+    }
+    result = `${roll} - ${pid} - ${name} - ${collegeid} - ${hid} - ${password} - ${eventid}`;
+    return res.status(200).json(result);
+};
--- server/routes/student_routes.js
+++ server/routes/student_routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAllStudents, getStudentByID, registerAsVolunteer } from '../controls/student_controls';
+import { getAllStudents, getStudentByID, registerAsVolunteer, registerForEvent } from '../controls/student_controls';
 
 const studentRouter = express.Router();
 
@@ -7,5 +7,6 @@ studentRouter.get("/", getAllStudents);
 
 studentRouter.get("/:id", getStudentByID);
 studentRouter.put("/volunteer", registerAsVolunteer);
+studentRouter.put("/register", registerForEvent);
 
 export default studentRouter;
