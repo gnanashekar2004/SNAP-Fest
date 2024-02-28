902f2ddefa7ecdf714816935e182234e98668579
Shekar2004 <gnanashekar2004@gmail.com>
2024-02-29 01:54:55 +0530

    added a background image

--- client/src/components/Homepage/card.js
+++ client/src/components/Homepage/card.js
@@ -2,11 +2,17 @@ import react from 'react';
 // import image from '../../assests/Admin.png';
 
 function Card({source, title , onClick}) {
+
+    const style1={
+        backgroundColor: "#48AAAD",
+        borderRadius: "0px 0px 15px 15px"
+    };
+
     return (
-    <div className="card" style={{width: "18rem"}} onClick={onClick}>
+    <div className="card" style={{width: "18rem", borderRadius: "15px"}} onClick={onClick}>
         <img src={source} class="card-img-top" alt={title} />
-        <div className="card-body">
-          <h3 className="card-text" style={{textAlign: "center"}}>{title}</h3>
+        <div className="card-body" style={style1}>
+          <h3 className="card-text" style={{textAlign: "center" , color: "white"}}>{title}</h3>
         </div>
       </div>
     );
